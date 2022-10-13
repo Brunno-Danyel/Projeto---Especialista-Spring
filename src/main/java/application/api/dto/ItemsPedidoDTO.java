b196be5076edde0b97624b249ebb1f85de2fd40a
Brunno Danyel <brunnodanyel25rbr@hotmail.com>
2022-10-13 11:30:16 -0300

    Created DTO

---
+++ src/main/java/application/api/dto/ItemsPedidoDTO.java
@@ -0,0 +1,12 @@
+package application.api.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ItemsPedidoDTO {
+
+    private Integer produto;
+    private Integer quantidade;
+}
---
+++ src/main/java/application/api/dto/PedidoDTO.java
@@ -0,0 +1,17 @@
+package application.api.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Getter
+@Setter
+public class PedidoDTO {
+
+    private Integer cliente;
+    private BigDecimal total;
+    private List<ItemsPedidoDTO> items;
+
+}
